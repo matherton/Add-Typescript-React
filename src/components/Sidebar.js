56dafbbc979d6c8bfbeb2125511b55767ac3edb6
Mark Atherton <mark@insightinternet.co.uk>
2022-09-03 19:42:22 +0100

    fixed jumping issue by removing classes from svg and adding them to the parent button instead

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,14 +5,11 @@ export default function Sidebar() {
   return (
     <>
       {!open ? (
-        <button onClick={() => setOpen(true)}>
-          <svg
-            className="fixed  z-30 flex items-center cursor-pointer right-10 top-6"
-            fill="#2563EB"
-            viewBox="0 0 100 80"
-            width="40"
-            height="40"
-          >
+        <button
+          className="fixed  z-30 flex items-center cursor-pointer right-10 top-6"
+          onClick={() => setOpen(true)}
+        >
+          <svg fill="#2563EB" viewBox="0 0 100 80" width="40" height="40">
             <rect width="100" height="10"></rect>
             <rect y="30" width="100" height="10"></rect>
             <rect y="60" width="100" height="10"></rect>
